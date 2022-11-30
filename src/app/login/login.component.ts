18931104202346c127e60a11f9f7b090735903da
alanbennyidk <alanbennyidk@gmail.com>
2022-11-30 19:21:34 +0530

    login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  aim="your perfect banking partner"
+  account="enter your account here"
+
+  
+  acno=''
+  pswd=''
+
+
+  userDetails:any={
+    1000:{acno:1000,username:"arun",password:123,balance:1000},
+    1001:{acno:1001,username:"amal",password:123,balance:1000},
+    1002:{acno:1002,username:"anil",password:123,balance:1000},
+
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  acnoChange(event:any)
+  {
+    console.log(event);
+    this.acno=event.target.value;
+    console.log(this.acno);
+    
+  }
+  pswdChange(event:any)
+  {
+    console.log(event);
+    this.pswd=event.target.value;
+    console.log(this.pswd);
+    
+  }
+  login(){
+    //alert('login clicked')
+    var acno=this.acno;
+    var pswd=this.pswd;
+    var userDetails=this.userDetails;
+
+    if(acno in userDetails){
+      if(pswd==userDetails[acno]['password']){
+        alert('Login successful')
+      }
+      else{
+        alert('Invalid password')
+      }
+    }
+    else{
+      alert('Invalid userdetails')
+    }
+  }
+
+}
